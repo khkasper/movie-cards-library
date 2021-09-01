a16cde0f63f3c842f5e41b06509dc2cded5a74ab
Kristiano <kristianokasper@gmail.com>
2021-09-01 18:14:05 -0300

    Requirement 15 and 16 ok; Requirement 3 correction; MovieList PropTypes ok

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
+    const { movies } = this.props;
     return (
       <div>
         {movies.map((movie) => (
@@ -12,5 +14,9 @@ class MovieList extends Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
