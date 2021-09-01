649e39b81ff5ca2d6d6cf4d66b3be204a7c57513
Kristiano <kristianokasper@gmail.com>
2021-09-01 16:37:53 -0300

    Requirement 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+export default class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        <MovieCard />
+      </div>
+    );
+  }
+}
