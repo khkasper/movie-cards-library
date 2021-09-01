dc965130a186c1f8abf32696360f5c426936cc60
Kristiano <kristianokasper@gmail.com>
2021-09-01 17:32:16 -0300

    Requirement 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 class Rating extends Component {
   render() {
+    const { rating } = this.props;
     return (
       <div>
-        
+        <div className="rating">{rating}</div>
       </div>
     );
   }
