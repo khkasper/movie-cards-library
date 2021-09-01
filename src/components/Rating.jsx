8ebe68e3785b422d385a409fee1d5eabfb79f6eb
Kristiano <kristianokasper@gmail.com>
2021-09-01 17:21:57 -0300

    Requirement 11

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Rating;
